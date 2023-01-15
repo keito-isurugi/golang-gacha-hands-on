3e7cb7c02b14d4ebc4c140cd2ee524eff11c613b
KeitoIsurugi <isurugi@csplan.jp>
2023-01-15 21:00:16 +0900

    first commit

---
+++ skeleton/section01/step03/main.go
@@ -0,0 +1,24 @@
+// STEP03: ランダムな数字の表示
+
+package main
+
+import (
+	// TODO: fmtパッケージをインポートする
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	// 乱数の種を設定する
+	// 現在時刻をUNIX時間にしたものを種とする
+	rand.Seed(time.Now().Unix())
+
+	// TODO: 0から9までの間で乱数を作り変数numに代入する
+	t := time.Now().Unix()
+	rand.Seed(t)
+	num := rand.Intn(10)
+	// 変数numを表示する
+	fmt.Println(num)
+}
